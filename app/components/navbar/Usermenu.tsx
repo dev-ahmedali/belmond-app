904c98566bcddba4a37f55424b2d1c0023d2047a
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-03 14:56:38 +0600

    navbar done

--- app/components/navbar/Usermenu.tsx
+++ app/components/navbar/Usermenu.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
           Book your home
         </div>
@@ -16,10 +23,22 @@ const UserMenu = () => {
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
           <AiOutlineMenu />
           <div className="md:block hidden">
-            <Avatar/>
+            <Avatar />
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="rounded-xl shadow-md absolute w-[40vw] md:w-3/4 bg-white right-0 overflow-hidden top-12 text-sm
+        ">
+          <div className="flex flex-col cursor-pointer">
+            <>
+            <MenuItem onClick={() => {}} label="Login"/>
+            <MenuItem onClick={() => {}} label="Sign up"/>
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
