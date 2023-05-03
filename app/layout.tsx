09fc8c637f039cd6e0436b9bdfa39ada54ae8f7b
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-03 17:37:22 +0600

    created modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal/>
+        <Modal title="HEllo world" isOpen/>
         <Navbar />
+        
         </ClientOnly>
         {children}
       </body>
