4c09d2a13ac113e9f0b0d23aa4ce7863f65dadd7
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-12 02:13:22 +0600

    updated listing category 3

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,6 +3,9 @@
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 enum STEPS {
   CATEGORY = 0,
@@ -18,26 +21,46 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const onBack = () => {
-    setSteps((value) => value - 1)
-  }
+    setSteps((value) => value - 1);
+  };
 
   const onNext = () => {
-    setSteps((value) => value + 1)
-  }
-  
+    setSteps((value) => value + 1);
+  };
+
   const actionLabel = useMemo(() => {
-    if(steps === STEPS.PRICE) {
-        return 'Create'
+    if (steps === STEPS.PRICE) {
+      return "Create";
     }
-    return 'Next'
-  }, [steps])
+    return "Next";
+  }, [steps]);
 
   const secondaryActionLabel = useMemo(() => {
-    if(steps === STEPS.CATEGORY){
-        return undefined
+    if (steps === STEPS.CATEGORY) {
+      return undefined;
     }
-    return 'Back'
-  }, [steps])
+    return "Back";
+  }, [steps]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+        <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"/>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh]
+        overflow-y-auto">
+            {categories.map((item) => (
+                <div className="col-span-1" key={item.label}>
+                    <CategoryInput
+                    onClick={() => {}}
+                    selected={false}
+                    label={item.label}
+                    icon={item.icon}/>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
   return (
     <Modal
       isOpen={rentModal.isOpen}
@@ -45,8 +68,9 @@ const RentModal = () => {
       onSubmit={rentModal.onClose}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
-      secondaryAction = {steps === STEPS.CATEGORY ? undefined : onBack}
+      secondaryAction={steps === STEPS.CATEGORY ? undefined : onBack}
       title="Belmond your home"
+      body={bodyContent}
     />
   );
 };
