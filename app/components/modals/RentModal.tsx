4c6a60cc43f11dae2da89343942ec93351aa2059
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-12 01:09:46 +0600

    updated rent modal

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import useRentModal from "@/app/hooks/useRentModal";
+import Modal from "./Modal";
+
+enum STEPS {
+    CATEGORY= 0,
+    LOCATION= 1,
+    INFO=2,
+    IMAGE=3,
+    DESCRIPTION=4,
+    PRICE=5
+}
+
+const RentModal = () => {
+  const rentModal = useRentModal();
+  return (
+    <Modal
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel="Submit"
+      title="Belmond your home"
+    />
+  );
+};
+
+export default RentModal;
