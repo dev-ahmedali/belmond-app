624818410927bb5308a5215722d2467790aa294d
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-11 10:41:14 +0600

    updated categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,28 +1,28 @@
 "use client";
 
 import Container from "../Container";
-import {TbBeach} from "react-icons/tb"
-import {GiWindmill} from "react-icons/gi"
-import {MdOutlineVilla} from 'react-icons/md'
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 
 export const categories = [
-    {
-        label: 'Windmlls',
-        icon: GiWindmill,
-        description: 'This property has windmills'
-    },
-    {
-        label:'beach',
-        icon: TbBeach,
-        description: 'This property has Tb beach'
-    },
-    {
-        label: 'modern',
-        icon: MdOutlineVilla,
-        description: 'This property has modern'
-    },
-]
+  {
+    label: "Windmlls",
+    icon: GiWindmill,
+    description: "This property has windmills",
+  },
+  {
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property has Tb beach",
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property has modern",
+  },
+];
 
 const Categories = () => {
   return (
@@ -30,20 +30,21 @@ const Categories = () => {
       <div
         className="
         pt-4
-        flex flex-row
+        flex 
+        flex-row
         items-center
-        justify-center overflow-x-auto
+        justify-center 
+        overflow-x-auto
     ">
-      {
-        categories.map((item) => (
-          <CategoryBox 
-          key={item.label}
-          label={item.label}
-          description={item.description}
-          icon={item.icon}/>
-        ))
-      }
-    </div>
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
