b097105d1a86800c7dae48dc0e3350065a8381ff
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-12 01:33:30 +0600

    updated rent modal 2

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,25 +1,51 @@
-'use client'
+"use client";
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+import { useMemo, useState } from "react";
 
 enum STEPS {
-    CATEGORY= 0,
-    LOCATION= 1,
-    INFO=2,
-    IMAGE=3,
-    DESCRIPTION=4,
-    PRICE=5
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGE = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
 }
 
 const RentModal = () => {
+  const [steps, setSteps] = useState(STEPS.CATEGORY);
   const rentModal = useRentModal();
+
+  const onBack = () => {
+    setSteps((value) => value - 1)
+  }
+
+  const onNext = () => {
+    setSteps((value) => value + 1)
+  }
+  
+  const actionLabel = useMemo(() => {
+    if(steps === STEPS.PRICE) {
+        return 'Create'
+    }
+    return 'Next'
+  }, [steps])
+
+  const secondaryActionLabel = useMemo(() => {
+    if(steps === STEPS.CATEGORY){
+        return undefined
+    }
+    return 'Back'
+  }, [steps])
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction = {steps === STEPS.CATEGORY ? undefined : onBack}
       title="Belmond your home"
     />
   );
