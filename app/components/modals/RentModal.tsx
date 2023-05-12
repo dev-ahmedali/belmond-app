00b0c0b86fc111916b406a798a2572f1c27c8c38
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-12 12:03:28 +0600

    updated listing category 4

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,6 +6,7 @@ import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import { FieldName, FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
   CATEGORY = 0,
@@ -19,6 +20,37 @@ enum STEPS {
 const RentModal = () => {
   const [steps, setSteps] = useState(STEPS.CATEGORY);
   const rentModal = useRentModal();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { error },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathRoomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
 
   const onBack = () => {
     setSteps((value) => value - 1);
@@ -44,23 +76,26 @@ const RentModal = () => {
 
   let bodyContent = (
     <div className="flex flex-col gap-8">
-        <Heading
+      <Heading
         title="Which of these best describes your place?"
-        subtitle="Pick a category"/>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh]
+        subtitle="Pick a category"
+      />
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh]
         overflow-y-auto">
-            {categories.map((item) => (
-                <div className="col-span-1" key={item.label}>
-                    <CategoryInput
-                    onClick={() => {}}
-                    selected={false}
-                    label={item.label}
-                    icon={item.icon}/>
-                </div>
-            ))}
-        </div>
+        {categories.map((item) => (
+          <div className="col-span-1" key={item.label}>
+            <CategoryInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
   return (
     <Modal
       isOpen={rentModal.isOpen}
