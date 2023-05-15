01bb81dd35cce6746d90f1cfa7c33041623f963a
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-15 17:00:03 +0600

    added upload image cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -160,11 +161,15 @@ const RentModal = () => {
   }
 
   if (steps === STEPS.IMAGE) {
-    bodyContent = <div className="flex flex-col gap-8">
-      <Heading
-      title="Add a photo of your place"
-      subtitle="Show guest what your place looks like!"/>
-    </div>;
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guest what your place looks like!"
+        />
+        <ImageUpload/>
+      </div>
+    );
   }
   return (
     <Modal
