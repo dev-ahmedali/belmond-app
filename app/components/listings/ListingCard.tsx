7364db6fd50290d0baaeb9c3a47b7c638a402840
dev-ahmedali <ahmedali69325@gmail.com>
2023-05-19 11:23:31 +0600

    updated listing card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
 
 interface ListingCardProps {
   data: Listing;
@@ -14,16 +16,39 @@ interface ListingCardProps {
   currentUser: SafeUser | null;
 }
 
-const ListingCard:React.FC<ListingCardProps> = ({
-    data,
-    reservation,
-    onAction,
-    disabled,
-    actionLabel,
-    actionId,
-    currentUser
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = "",
+  currentUser,
 }) => {
-    const router = useRouter()
+  const router = useRouter();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled],
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice
+    }
+    return data.price
+  }, [reservation, data.price]);
+
   return <div>Listing card</div>;
 };
 
